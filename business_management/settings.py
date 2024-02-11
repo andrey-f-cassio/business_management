ddc057211e07968a0733a8513d7b93ad4b9a825c
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-11 05:31:20 -0300

    Feat: Add companies app to INSTALLED_APPS.

--- business_management/settings.py
+++ business_management/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    "companies.apps.CompaniesConfig"
 ]
 
 MIDDLEWARE = [
