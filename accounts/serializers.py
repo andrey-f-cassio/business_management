1f3654330e77091ee1d7c4fca86f0b2e41231bab
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-11 04:51:23 -0300

    Feat: Add password and extra_kwargs to Meta class.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,6 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -13,5 +16,6 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
+            password=validated_data['password']
         )
         return user
