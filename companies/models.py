ba5d3d597cff74089a8c70d788f1e4e362edc3b5
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 13:26:17 -0300

    Feat: Add str method to return company name.

--- companies/models.py
+++ companies/models.py
@@ -7,3 +7,6 @@ class Company(models.Model):
     cnpj = models.CharField(max_length=20)
     corporate_name = models.CharField(max_length=100)
     trade_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.trade_name
