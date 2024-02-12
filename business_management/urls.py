a42e96d0e2dcb0e4120d2523de1c88c31673a608
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 16:42:14 -0300

    feat: add swagger docs to urlpatterns.

--- business_management/urls.py
+++ business_management/urls.py
@@ -16,9 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Business Management",
+      default_version='v1',
+      description="business management",
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/companies/', include('companies.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
