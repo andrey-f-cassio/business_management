01c5996b36dec707d966dccf163f6366c8a09a43
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 15:24:51 -0300

    refactor: using DefaultRouter instead a manual route configuring.

--- companies/urls.py
+++ companies/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import CompanyViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CompanyViewSet, CompanyMemberViewSet
+
+router = DefaultRouter()
+
+router.register(r'', CompanyViewSet, basename='company')
+router.register(r'members', CompanyMemberViewSet, basename='company-member')
 
 urlpatterns = [
-    path('create/', CompanyViewSet.as_view({'post': 'create'}), name='company-create'),
+    path('', include(router.urls)),
 ]
