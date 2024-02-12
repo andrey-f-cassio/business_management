164d9fcadd29fac6af203f8bfc2867ebbca8cede
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 11:43:31 -0300

    Feat: Add Company class and a post method to create a new companie.

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .serializers import CompanySerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+
+class Company(APIView):
+
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
