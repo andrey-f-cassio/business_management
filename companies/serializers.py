095e259d2bc507438937b12a4a852ccbfd4ffaec
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 15:25:49 -0300

    feat: Add CompanyMemberSerializer.

--- companies/serializers.py
+++ companies/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, CompanyMember
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+
+class CompanyMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyMember
+        fields = ['id', 'user', 'company', 'date_joined']
