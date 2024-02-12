f8f0532d68a67002543ae9b9709496cf69a9ce0f
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 16:41:43 -0300

    feat: add drf_yasg to INSTALLED_APPS.

--- business_management/settings.py
+++ business_management/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'rest_framework.authtoken',
-    "companies.apps.CompaniesConfig"
+    "companies.apps.CompaniesConfig",
+    "drf_yasg"
 ]
 
 MIDDLEWARE = [
