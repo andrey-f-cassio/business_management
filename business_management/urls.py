fbd299e040f1c31528a0acd901da1387b411c9c5
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 17:54:07 -0300

    feat: add company-mebers to urlpatterns.

--- business_management/urls.py
+++ business_management/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/companies/', include('companies.urls')),
+    path('api/company-members/', include('companies.urls_members')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
