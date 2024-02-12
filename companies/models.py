bff8f6abfee34524d2384bfe836e7d647105a96d
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 10:34:43 -0300

    Feat: Add Company model.

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Company(models.Model):
+    owner = models.ForeignKey(User, related_name='companies', on_delete=models.CASCADE)
+    cnpj = models.CharField(max_length=20)
+    corporate_name = models.CharField(max_length=100)
+    trade_name = models.CharField(max_length=100)
