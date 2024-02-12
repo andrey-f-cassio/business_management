1b9b77d0d51c173e82a49688d2eb517c6b370b57
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 13:46:59 -0300

    refactor: Change to CompanyViewSet .

--- companies/views.py
+++ companies/views.py
@@ -1,14 +1,8 @@
+from rest_framework import viewsets
+from .models import Company
 from .serializers import CompanySerializer
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 
 
-class Company(APIView):
-
-    def post(self, request):
-        serializer = CompanySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
