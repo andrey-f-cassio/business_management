7a9af6d6e062597f8c947e81bd3252c616245b2d
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-12 17:54:48 -0300

    refactor: Remove sub-route members.

--- companies/urls.py
+++ companies/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet, CompanyMemberViewSet
+from .views import CompanyViewSet
 
 router = DefaultRouter()
 
 router.register(r'', CompanyViewSet, basename='company')
-router.register(r'members', CompanyMemberViewSet, basename='company-member')
 
 urlpatterns = [
     path('', include(router.urls)),
