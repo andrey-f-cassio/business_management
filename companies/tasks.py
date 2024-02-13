403f9f3c42a00f00ffc20ee4249975b7d2d40136
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-13 17:56:45 -0300

    refactor: Add condition to check if the company has not yet been updated since the creation date or if 30 days have passed since its last update, to be updated again.

--- companies/tasks.py
+++ companies/tasks.py
@@ -5,15 +5,16 @@ from .models import Company
 
 
 def update_company_data():
-    # month_ago = timezone.now() - timezone.timedelta(minutes=1)
-    month_ago = timezone.now() - timezone.timedelta(days=30)
-    companies_to_update = Company.objects.filter(created_at__lte=month_ago)
+    current_time = timezone.now()
+    companies_to_update = Company.objects.all()
 
     for company in companies_to_update:
-        response = requests.get(f'https://receitaws.com.br/v1/cnpj/{company.cnpj}')
-        if response.status_code == 200:
-            data = response.json()
-            company.corporate_name = data.get('nome')
-            company.trade_name = data.get('fantasia')
-            company.status = data.get('status')
-            company.save()
+        if not company.last_updated or (current_time - company.last_updated).days > 30:
+            response = requests.get(f'https://receitaws.com.br/v1/cnpj/{company.cnpj}')
+            if response.status_code == 200:
+                data = response.json()
+                company.corporate_name = data.get('nome', company.corporate_name)
+                company.trade_name = data.get('fantasia', company.trade_name)
+                company.status = data.get('status', company.status)
+                company.last_updated = current_time
+                company.save()
