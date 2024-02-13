53212c0fa20b59bc388de63860a5166c498eea8d
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-13 17:17:16 -0300

    feat: Add update_company_data function to tasks script.

--- companies/tasks.py
+++ companies/tasks.py
@@ -1,12 +1,19 @@
 import requests
-from datetime import datetime, timedelta
+from django.utils import timezone
 
 from .models import Company
 
 
 def update_company_data():
-    print("rodando!")
-    # response = requests.get(f"https://receitaws.com.br/v1/cnpj/13330765000131")
-    # return response.json()
+    # month_ago = timezone.now() - timezone.timedelta(minutes=1)
+    month_ago = timezone.now() - timezone.timedelta(days=30)
+    companies_to_update = Company.objects.filter(created_at__lte=month_ago)
 
-
+    for company in companies_to_update:
+        response = requests.get(f'https://receitaws.com.br/v1/cnpj/{company.cnpj}')
+        if response.status_code == 200:
+            data = response.json()
+            company.corporate_name = data.get('nome')
+            company.trade_name = data.get('fantasia')
+            company.status = data.get('status')
+            company.save()
