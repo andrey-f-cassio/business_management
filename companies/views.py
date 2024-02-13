1fcf972cfc8eb0cb883558276e9305c211aaf5cf
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-13 00:23:15 -0300

    feat: Add permission_classes to CompanyMemberViewSet and CompanyViewSet and add get_queryset to both.

--- companies/views.py
+++ companies/views.py
@@ -1,13 +1,23 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Company, CompanyMember
 from .serializers import CompanySerializer, CompanyMemberSerializer
+from django.shortcuts import get_object_or_404
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
-    queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Company.objects.filter(user=self.request.user)
 
 
 class CompanyMemberViewSet(viewsets.ModelViewSet):
-    queryset = CompanyMember.objects.all()
     serializer_class = CompanyMemberSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        company_id = self.kwargs.get('pk')
+        company = get_object_or_404(Company, pk=company_id)
+        return CompanyMember.objects.filter(company=company)
