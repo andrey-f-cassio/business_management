44ea14967688e99b90b1743bdb8bccb0ea308aa7
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-13 15:53:06 -0300

    refactor: remove celery script to project.

--- business_management/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'business_management.settings')
-
-app = Celery('business_management')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
