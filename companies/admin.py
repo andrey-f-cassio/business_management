b63767f5467c5f25f70c935cb6722bf3a2c95259
Andrey Freitas Cassio <andreyfcassio@gmail.com>
2024-02-14 02:19:14 -0300

    refactor: start from zero to create PR.

--- companies/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from companies.models import Company, CompanyMember
-
-# Register your models here.
-admin.site.register(Company)
-admin.site.register(CompanyMember)
